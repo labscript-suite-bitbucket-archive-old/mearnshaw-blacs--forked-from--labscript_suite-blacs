d625b03eaac4fa87668d5272a4bf8b487a06096a
Matt Earnshaw <matt@earnshaw.org.uk>
2014-08-27 18:30:41 +0100

    include_package_data in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 setup(
         name = "blacs",
         packages = find_packages(),
+        include_package_data = True,
         install_requires = ['h5py', 'zprocess', 'qtutils']
 )
 
